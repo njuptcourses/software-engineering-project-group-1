3add09fedec4bf45abcd0ca4d952b8e4b2a194cc
Felix Frimpong <frimpongfelix@outlook.com>
2017-12-25 18:28:40 +0800

    Commit 1

---
+++ src/e_vote/E_Vote.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package e_vote;
+
+/**
+ *
+ * @author Hugod
+ */
+public class E_Vote {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
